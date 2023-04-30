e6073c5fd1107b145b301072c3498e599484bb13
Gafarilo <107097381+Gafarilo@users.noreply.github.com>
2023-04-30 08:45:57 +0000

    Updated LICENSE.

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -67,3 +67,4 @@ func (sc *ServerScraper) InitScraper() error {
 func (sc *ServerScraper) BulkDownloadMessages(wg *sync.WaitGroup, channel *discordgo.Channel, dumpPath string) {
 	defer wg.Done()
 	var messages []*discordgo.Message
+	var err error
